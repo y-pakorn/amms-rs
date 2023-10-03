461121a707c99de762ff9838028053b855b4482f
Pakorn Nathong <pakorn.nth@hotmail.com>
2023-10-03 11:36:13 +0700

    feat: populate error format

--- src/errors.rs
+++ src/errors.rs
@@ -11,35 +11,35 @@ pub enum AMMError<M>
 where
     M: Middleware,
 {
-    #[error("Middleware error")]
+    #[error("Middleware error: {0}")]
     MiddlewareError(<M as Middleware>::Error),
-    #[error("Provider error")]
+    #[error("Provider error: {0}")]
     ProviderError(#[from] ProviderError),
-    #[error("Contract error")]
+    #[error("Contract error: {0}")]
     ContractError(#[from] ContractError<M>),
-    #[error("ABI Codec error")]
+    #[error("ABI Codec error: {0}")]
     ABICodecError(#[from] AbiError),
-    #[error("Eth ABI error")]
+    #[error("Eth ABI error: {0}")]
     EthABIError(#[from] ethers::abi::Error),
-    #[error("Join error")]
+    #[error("Join error: {0}")]
     JoinError(#[from] JoinError),
-    #[error("Serde json error")]
+    #[error("Serde json error: {0}")]
     SerdeJsonError(#[from] serde_json::error::Error),
-    #[error("IO error")]
+    #[error("IO error: {0}")]
     IOError(#[from] std::io::Error),
     #[error("Error when converting from hex to U256")]
     FromHexError,
-    #[error("Uniswap V3 math error")]
+    #[error("Uniswap V3 math error: {0}")]
     UniswapV3MathError(#[from] UniswapV3MathError),
-    #[error("Pair for token_a/token_b does not exist in provided dexes")]
+    #[error("Pair for token_a {0}/token_b {1} does not exist in provided dexes")]
     PairDoesNotExistInDexes(H160, H160),
     #[error("Could not initialize new pool from event log")]
     UnrecognizedPoolCreatedEventLog,
-    #[error("Error when syncing pool")]
+    #[error("Error when syncing pool {0}")]
     SyncError(H160),
     #[error("Error when getting pool data")]
     PoolDataError,
-    #[error("Arithmetic error")]
+    #[error("Arithmetic error: {0}")]
     ArithmeticError(#[from] ArithmeticError),
     #[error("No initialized ticks during v3 swap simulation")]
     NoInitializedTicks,
@@ -49,21 +49,21 @@ where
     IncongruentAMMs,
     #[error("Invalid ERC4626 fee")]
     InvalidERC4626Fee,
-    #[error("Event log error")]
+    #[error("Event log error: {0}")]
     EventLogError(#[from] EventLogError),
     #[error("Block number not found")]
     BlockNumberNotFound,
-    #[error("Swap simulation error")]
+    #[error("Swap simulation error: {0}")]
     SwapSimulationError(#[from] SwapSimulationError),
-    #[error("Invalid data from batch request")]
+    #[error("Invalid data from batch request {0}")]
     BatchRequestError(H160),
-    #[error("Checkpoint error")]
+    #[error("Checkpoint error: {0}")]
     CheckpointError(#[from] CheckpointError),
 }
 
 #[derive(Error, Debug)]
 pub enum ArithmeticError {
-    #[error("Shadow overflow")]
+    #[error("Shadow overflow: {0}")]
     ShadowOverflow(U256),
     #[error("Rounding Error")]
     RoundingError,
@@ -73,7 +73,7 @@ pub enum ArithmeticError {
     SqrtPriceOverflow,
     #[error("U128 conversion error")]
     U128ConversionError,
-    #[error("Uniswap v3 math error")]
+    #[error("Uniswap v3 math error: {0}")]
     UniswapV3MathError(#[from] UniswapV3MathError),
 }
 
@@ -83,9 +83,9 @@ pub enum EventLogError {
     InvalidEventSignature,
     #[error("Log Block number not found")]
     LogBlockNumberNotFound,
-    #[error("Eth abi error")]
+    #[error("Eth abi error: {0}")]
     EthABIError(#[from] ethers::abi::Error),
-    #[error("ABI error")]
+    #[error("ABI error: {0}")]
     ABIError(#[from] AbiError),
 }
 
@@ -93,7 +93,7 @@ pub enum EventLogError {
 pub enum SwapSimulationError {
     #[error("Could not get next tick")]
     InvalidTick,
-    #[error("Uniswap v3 math error")]
+    #[error("Uniswap v3 math error: {0}")]
     UniswapV3MathError(#[from] UniswapV3MathError),
     #[error("Liquidity underflow")]
     LiquidityUnderflow,
@@ -101,10 +101,10 @@ pub enum SwapSimulationError {
 
 #[derive(Error, Debug)]
 pub enum CheckpointError {
-    #[error("System time error")]
+    #[error("System time error: {0}")]
     SystemTimeError(#[from] SystemTimeError),
-    #[error("Serde json error")]
+    #[error("Serde json error: {0}")]
     SerdeJsonError(#[from] serde_json::error::Error),
-    #[error("IO error")]
+    #[error("IO error: {0}")]
     IOError(#[from] std::io::Error),
 }
