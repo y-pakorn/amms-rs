c50cac92fcc0626c81b16a839218790dd99fa057
Pakorn Nathong <pakorn.nth@hotmail.com>
2023-10-05 01:48:59 +0700

    feat: use join all

--- src/sync/checkpoint.rs
+++ src/sync/checkpoint.rs
@@ -5,9 +5,10 @@ use std::{
 };
 
 use ethers::{providers::Middleware, types::H160};
+use futures::future::try_join_all;
 use indicatif::ProgressBar;
 use serde::{Deserialize, Serialize};
-use tokio::task::{JoinHandle, JoinSet};
+use tokio::task::JoinHandle;
 
 use crate::{
     amm::{
@@ -72,29 +73,24 @@ pub async fn sync_amms_from_checkpoint<M: 'static + Middleware>(
     //Sort all of the pools from the checkpoint into uniswap_v2_pools and uniswap_v3_pools pools so we can sync them concurrently
     let (uniswap_v2_pools, uniswap_v3_pools, erc_4626_pools) = sort_amms(checkpoint.amms);
 
-    let mut aggregated_amms = vec![];
-    let mut handles = JoinSet::new();
+    let mut handles = vec![];
 
     //Sync all uniswap v2 pools from checkpoint
     if !uniswap_v2_pools.is_empty() {
-        batch_sync_amms_from_checkpoint(
-            &mut handles,
+        handles.extend(batch_sync_amms_from_checkpoint(
             uniswap_v2_pools,
             current_block,
             middleware.clone(),
-        )
-        .await?;
+        )?);
     }
 
     //Sync all uniswap v3 pools from checkpoint
     if !uniswap_v3_pools.is_empty() {
-        batch_sync_amms_from_checkpoint(
-            &mut handles,
+        handles.extend(batch_sync_amms_from_checkpoint(
             uniswap_v3_pools,
             current_block,
             middleware.clone(),
-        )
-        .await?;
+        )?);
     }
 
     if !erc_4626_pools.is_empty() {
@@ -106,19 +102,24 @@ pub async fn sync_amms_from_checkpoint<M: 'static + Middleware>(
     }
 
     //Sync all pools from the since synced block
-    get_new_amms_from_range(
-        &mut handles,
+    handles.extend(get_new_amms_from_range(
         checkpoint.factories.clone(),
         checkpoint.block_number,
         current_block,
         step,
         middleware.clone(),
-    )
-    .await?;
+    )?);
 
-    while let Some(amms) = handles.join_next().await {
-        aggregated_amms.extend(amms??);
-    }
+    let aggregated_amms = try_join_all(handles)
+        .await?
+        .into_iter()
+        .flat_map(|amms| match amms {
+            Ok(amms) => amms.into_iter().map(Ok).collect(),
+            Err(err) => {
+                vec![Err(err)]
+            }
+        })
+        .collect::<Result<Vec<_>, _>>()?;
 
     //update the sync checkpoint
     construct_checkpoint(
@@ -133,16 +134,16 @@ pub async fn sync_amms_from_checkpoint<M: 'static + Middleware>(
     Ok((checkpoint.factories, aggregated_amms))
 }
 
-pub async fn get_new_amms_from_range<M: 'static + Middleware>(
-    handles: &mut JoinSet<Result<Vec<AMM>, AMMError<M>>>,
+pub fn get_new_amms_from_range<M: 'static + Middleware>(
     factories: Vec<Factory>,
     from_block: u64,
     to_block: u64,
     step: u64,
     middleware: Arc<M>,
-) -> Result<(), AMMError<M>> {
+) -> Result<Vec<JoinHandle<Result<Vec<AMM>, AMMError<M>>>>, AMMError<M>> {
     //Create the filter with all the pair created events
     //Aggregate the populated pools from each thread
+    let mut handles = vec![];
     for factory in factories.into_iter() {
         let middleware = middleware.clone();
         let spinner = MULTIPROGRESS.add(
@@ -153,7 +154,7 @@ pub async fn get_new_amms_from_range<M: 'static + Middleware>(
         spinner.enable_steady_tick(Duration::from_millis(200));
 
         //Spawn a new thread to get all pools and sync data for each dex
-        handles.spawn(async move {
+        handles.push(tokio::spawn(async move {
             let mut amms = factory
                 .get_all_pools_from_logs(from_block, to_block, step, middleware.clone())
                 .await?;
@@ -167,18 +168,17 @@ pub async fn get_new_amms_from_range<M: 'static + Middleware>(
 
             spinner.finish_and_clear();
             Ok::<_, AMMError<M>>(amms)
-        });
+        }));
     }
 
-    Ok(())
+    Ok(handles)
 }
 
-pub async fn batch_sync_amms_from_checkpoint<M: 'static + Middleware>(
-    handles: &mut JoinSet<Result<Vec<AMM>, AMMError<M>>>,
+pub fn batch_sync_amms_from_checkpoint<M: 'static + Middleware>(
     amms: Vec<AMM>,
     block_number: u64,
     middleware: Arc<M>,
-) -> Result<(), AMMError<M>> {
+) -> Result<Vec<JoinHandle<Result<Vec<AMM>, AMMError<M>>>>, AMMError<M>> {
     let factory = match amms[0] {
         AMM::UniswapV2Pool(_) => Some(Factory::UniswapV2Factory(UniswapV2Factory::new(
             H160::zero(),
@@ -193,6 +193,7 @@ pub async fn batch_sync_amms_from_checkpoint<M: 'static + Middleware>(
 
         AMM::ERC4626Vault(_) => None,
     };
+    let mut handles = vec![];
 
     //Spawn a new thread to get all pools and sync data for each dex
     if let Some(_factory) = factory {
@@ -200,7 +201,7 @@ pub async fn batch_sync_amms_from_checkpoint<M: 'static + Middleware>(
             for amms in amms.chunks(50_000) {
                 let mut amms = amms.to_vec();
                 let middleware = middleware.clone();
-                handles.spawn(async move {
+                handles.push(tokio::spawn(async move {
                     //Get all pool data via batched calls
                     amms = populate_amms(&amms, block_number, None, middleware).await?;
                     //factory
@@ -209,15 +210,13 @@ pub async fn batch_sync_amms_from_checkpoint<M: 'static + Middleware>(
                     //Clean empty pools
                     amms = sync::remove_empty_amms(amms);
                     Ok::<_, AMMError<M>>(amms)
-                });
+                }));
             }
-            Ok(())
         } else {
-            Err(AMMError::IncongruentAMMs)
+            Err(AMMError::IncongruentAMMs)?;
         }
-    } else {
-        Ok(())
     }
+    Ok(handles)
 }
 
 pub fn sort_amms(amms: Vec<AMM>) -> (Vec<AMM>, Vec<AMM>, Vec<AMM>) {
@@ -235,7 +234,7 @@ pub fn sort_amms(amms: Vec<AMM>) -> (Vec<AMM>, Vec<AMM>, Vec<AMM>) {
     (uniswap_v2_pools, uniswap_v3_pools, erc_4626_vaults)
 }
 
-pub async fn get_new_pools_from_range<M: 'static + Middleware>(
+pub fn get_new_pools_from_range<M: 'static + Middleware>(
     factories: Vec<Factory>,
     from_block: u64,
     to_block: u64,
